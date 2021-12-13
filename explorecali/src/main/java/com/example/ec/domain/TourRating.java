36dbda1897533f74b98f4aa1916ec751bdcf249c
4ixSer <qny4ix08@gmail.com>
2021-12-13 11:36:53 +0200

    Add Explorecali

---
+++ explorecali/src/main/java/com/example/ec/domain/TourRating.java
@@ -0,0 +1,116 @@
+package com.example.ec.domain;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "tour_rating")
+public class TourRating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "tour_id")
+    private Tour tour;
+
+    @Column(name = "customer_id")
+    private Integer customerId;
+
+    @Column(nullable = false)
+    private Integer score;
+
+    @Column
+    private String comment;
+
+    protected TourRating() {
+    }
+
+
+    public TourRating(Tour tour, Integer customerId, Integer score, String comment) {
+        this.tour = tour;
+        this.customerId = customerId;
+        this.score = score;
+        this.comment = comment;
+    }
+
+
+    public TourRating(Tour tour, Integer customerId, Integer score) {
+        this.tour = tour;
+        this.customerId = customerId;
+        this.score = score;
+        this.comment = toComment(score);
+    }
+
+
+    private String toComment(Integer score) {
+        switch (score) {
+            case 1:
+                return "Terrible";
+            case 2:
+                return "Poor";
+            case 3:
+                return "Fair";
+            case 4:
+                return "Good";
+            case 5:
+                return "Great";
+            default:
+                return score.toString();
+        }
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Tour getTour() {
+        return tour;
+    }
+
+    public void setTour(Tour tour) {
+        this.tour = tour;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TourRating that = (TourRating) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(tour, that.tour) &&
+                Objects.equals(customerId, that.customerId) &&
+                Objects.equals(score, that.score) &&
+                Objects.equals(comment, that.comment);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, tour, customerId, score, comment);
+    }
+}
